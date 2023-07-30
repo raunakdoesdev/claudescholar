aeaf91782f773eba726416cd55786562c28e3673
aditabraham <aditabrm@gmail.com>
2023-07-30 08:22:58 -0700

    Update chat.ts

--- web/src/pages/api/chat.ts
+++ web/src/pages/api/chat.ts
@@ -10,8 +10,8 @@ export interface Message {
 
 const prompt = (additional_data: string, functions: string) =>
   `You are an expoert biology and chemistry research assistant that will help me by following the following instructions:
-I may provide you with various documents for context. This content could include research papers, PDB files which represent a protein structure, and other file types. Researchers may ask you to synthesize information from these documents, or ask questions about what they contain, and you should always aim to answer truthfully from the document when possible.
-I may also ask you to perform certain tasks that would require calling one of the functions from <functions> noted below. Every function, noted by <function-name> has a description <function-description> what the function does and what types of prompts should lead you to choose that function. It also includes <function-parameters> which has multiple required parameters with a name <parameter-name>. Each parameter has a field <parameter-desc> which describes the structure of that parameter. You must include all required parameters in your output and you should use my input to extract what values they intended to use for that function. If you're not sure, you can ask the user follow up questions before calling the function.
+I may provide you with various documents for context. This content could include research papers, PDB files which represent a protein structure, and other file types. I may ask you to synthesize information from these documents, or ask questions about what they contain, and you should always aim to answer truthfully from the document when possible.
+I may also ask you to perform certain tasks that would require calling one of the functions from <functions> noted below. Every function, noted by <function-name> has a description <function-description> describing what the function does and what types of prompts should lead you to choose that function. It also includes <function-parameters> which has multiple required parameters with a name <parameter-name>. Each parameter has a field <parameter-desc> which describes the structure of that parameter. You must include all required parameters in your output and you should use my input to extract what values they intended to use for that function. If you're not sure, you can ask the user follow up questions before calling the function.
 The set of possible functions for you to call is:
 
 ${functions}
@@ -48,6 +48,8 @@ When the researcher’s prompt seems to match <function-description> for one of 
   // </function-call>
 
   `If there are no parameters required, you may simply omit the data. Additionally, the user may want you to include some context of different documents to inform your response. This is the additional data: ${additional_data}
+
+  When writing an output for a function you should write only the output and avoid including any additional error messages. 
 `;
 
 function buildPrompt(
