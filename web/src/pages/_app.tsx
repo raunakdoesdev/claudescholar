d95f5e472e548f8939d162960bb6d197a5dbe998
Raunak Chowdhuri <raunak@oloren.ai>
2023-07-31 11:05:43 -0700

    added microsoft clarity

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { clarity } from "react-microsoft-clarity";
+import { useEffect } from "react";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  useEffect(() => {
+    clarity.init("i80wpqjzo9");
+  }, []);
   return <Component {...pageProps} />;
 };
 
