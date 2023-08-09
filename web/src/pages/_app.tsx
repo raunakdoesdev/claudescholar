944f4988ac31398d2fd4b496319f1767bad3aceb
tunahfishy <tinahh@mit.edu>
2023-08-09 09:52:30 -0700

    clarity

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,18 +1,18 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
-// import { clarity } from "react-microsoft-clarity";
+import { clarity } from "react-microsoft-clarity";
 import { type Session } from "next-auth";
-// import { useEffect } from "react";
+import { useEffect } from "react";
 import { SessionProvider } from "next-auth/react";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
-  // useEffect(() => {
-  //   clarity.init("i80wpqjzo9");
-  // }, []);
+  useEffect(() => {
+    clarity.init("i80wpqjzo9");
+  }, []);
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
